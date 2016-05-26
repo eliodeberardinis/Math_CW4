ff0f6d36d308cca463fd271bc72d590ecf9e07ca
Elio de Berardinis <eliodeb@gmail.com>
2016-05-26 16:37:12 +0100

    cccc

---
+++ OpenGL_Simple_Program/Math_CW4/inc/Tutorial1PCH.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+
+//Important to include GLEW before freeGLUT bc freeGLUT includes the OpenGL header files
+#define GLEW_STATIC //macro used to link Glew static library
+#include <GL/glew.h>
+#include <GL/wglew.h> // For wglSwapInterval
+
+#define FREEGLUT_STATIC //macro needed to link to freeGLUT static library
+#include <GL/freeglut.h>
+
+#define GLM_FORCE_RADIANS //To force GLM to use radians for angles calculations
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/quaternion.hpp>
